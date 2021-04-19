25188b0d4ca296a6d940dbff7f249a404f8f3717
Hamilton Solis <hamiltonrafael1995@gmail.com>
2021-04-19 12:57:03 -0600

    Agregar Nuevo Empleado

---
+++ WindowsFormsUI/Formularios/FrmAgregarEmpleado.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsUI.Formularios
+{
+    public partial class FrmAgregarEmpleado : Form
+    {
+        public FrmAgregarEmpleado()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmAgregarEmpleado_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
