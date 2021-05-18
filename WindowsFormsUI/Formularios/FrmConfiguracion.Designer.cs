88d52552a0447b44514891e85d4095c46261e8cb
srquike <srquike@outlook.com>
2021-05-18 15:07:53 -0600

    Se agregaron las opciones de Respaldo y Restauracion de la base de datos

--- WindowsFormsUI/Formularios/FrmConfiguracion.Designer.cs
+++ WindowsFormsUI/Formularios/FrmConfiguracion.Designer.cs
@@ -38,37 +38,46 @@ namespace WindowsFormsUI.Formularios
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.TpBaseDatos = new System.Windows.Forms.TabPage();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.BtnRestaurar = new System.Windows.Forms.Button();
+            this.LblPorcentaje = new System.Windows.Forms.Label();
+            this.LblEstado = new System.Windows.Forms.Label();
+            this.PbProgreso = new System.Windows.Forms.ProgressBar();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.BtnRespaldar = new System.Windows.Forms.Button();
             this.BtnCancelar = new System.Windows.Forms.Button();
-            this.BtnCrearUsuario = new System.Windows.Forms.Button();
+            this.BtnGuardar = new System.Windows.Forms.Button();
+            this.FbdUbicacion = new System.Windows.Forms.FolderBrowserDialog();
+            this.OfdSeleccionar = new System.Windows.Forms.OpenFileDialog();
             this.TcOpciones.SuspendLayout();
             this.TpGeneral.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            this.TpBaseDatos.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // TcOpciones
             // 
             this.TcOpciones.Controls.Add(this.TpGeneral);
             this.TcOpciones.Controls.Add(this.TpBaseDatos);
-            this.TcOpciones.Dock = System.Windows.Forms.DockStyle.Fill;
             this.TcOpciones.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.TcOpciones.ItemSize = new System.Drawing.Size(100, 25);
             this.TcOpciones.Location = new System.Drawing.Point(0, 0);
             this.TcOpciones.Name = "TcOpciones";
             this.TcOpciones.SelectedIndex = 0;
-            this.TcOpciones.Size = new System.Drawing.Size(577, 450);
+            this.TcOpciones.Size = new System.Drawing.Size(577, 397);
             this.TcOpciones.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
             this.TcOpciones.TabIndex = 0;
             // 
             // TpGeneral
             // 
-            this.TpGeneral.Controls.Add(this.BtnCancelar);
-            this.TpGeneral.Controls.Add(this.BtnCrearUsuario);
             this.TpGeneral.Controls.Add(this.groupBox1);
             this.TpGeneral.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.TpGeneral.Location = new System.Drawing.Point(4, 29);
             this.TpGeneral.Name = "TpGeneral";
             this.TpGeneral.Padding = new System.Windows.Forms.Padding(3);
-            this.TpGeneral.Size = new System.Drawing.Size(569, 417);
+            this.TpGeneral.Size = new System.Drawing.Size(569, 364);
             this.TpGeneral.TabIndex = 0;
             this.TpGeneral.Text = "General";
             this.TpGeneral.UseVisualStyleBackColor = true;
@@ -87,7 +96,6 @@ namespace WindowsFormsUI.Formularios
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Cambiar contraseña de administrador";
-            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
             // ChkVerClaves
             // 
@@ -143,14 +151,99 @@ namespace WindowsFormsUI.Formularios
             // 
             // TpBaseDatos
             // 
+            this.TpBaseDatos.Controls.Add(this.groupBox3);
+            this.TpBaseDatos.Controls.Add(this.LblPorcentaje);
+            this.TpBaseDatos.Controls.Add(this.LblEstado);
+            this.TpBaseDatos.Controls.Add(this.PbProgreso);
+            this.TpBaseDatos.Controls.Add(this.groupBox2);
             this.TpBaseDatos.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.TpBaseDatos.Location = new System.Drawing.Point(4, 29);
             this.TpBaseDatos.Name = "TpBaseDatos";
             this.TpBaseDatos.Padding = new System.Windows.Forms.Padding(3);
-            this.TpBaseDatos.Size = new System.Drawing.Size(569, 417);
+            this.TpBaseDatos.Size = new System.Drawing.Size(569, 364);
             this.TpBaseDatos.TabIndex = 1;
             this.TpBaseDatos.Text = "Base de datos";
             this.TpBaseDatos.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.BtnRestaurar);
+            this.groupBox3.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox3.Location = new System.Drawing.Point(8, 75);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(553, 66);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Restaurar base de datos";
+            // 
+            // BtnRestaurar
+            // 
+            this.BtnRestaurar.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BtnRestaurar.Location = new System.Drawing.Point(6, 26);
+            this.BtnRestaurar.Name = "BtnRestaurar";
+            this.BtnRestaurar.Size = new System.Drawing.Size(89, 33);
+            this.BtnRestaurar.TabIndex = 1;
+            this.BtnRestaurar.Text = "Restaurar";
+            this.BtnRestaurar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnRestaurar.UseVisualStyleBackColor = true;
+            this.BtnRestaurar.Click += new System.EventHandler(this.BtnRestaurar_Click);
+            // 
+            // LblPorcentaje
+            // 
+            this.LblPorcentaje.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LblPorcentaje.AutoSize = true;
+            this.LblPorcentaje.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LblPorcentaje.Location = new System.Drawing.Point(259, 333);
+            this.LblPorcentaje.Name = "LblPorcentaje";
+            this.LblPorcentaje.Size = new System.Drawing.Size(33, 20);
+            this.LblPorcentaje.TabIndex = 5;
+            this.LblPorcentaje.Text = "0 %";
+            this.LblPorcentaje.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // LblEstado
+            // 
+            this.LblEstado.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LblEstado.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LblEstado.Location = new System.Drawing.Point(8, 310);
+            this.LblEstado.Name = "LblEstado";
+            this.LblEstado.Size = new System.Drawing.Size(553, 20);
+            this.LblEstado.TabIndex = 4;
+            this.LblEstado.Text = "Estado :";
+            this.LblEstado.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // PbProgreso
+            // 
+            this.PbProgreso.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PbProgreso.Location = new System.Drawing.Point(8, 332);
+            this.PbProgreso.Name = "PbProgreso";
+            this.PbProgreso.Size = new System.Drawing.Size(553, 23);
+            this.PbProgreso.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.BtnRespaldar);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox2.Location = new System.Drawing.Point(8, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(553, 66);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Respaldar base de datos";
+            // 
+            // BtnRespaldar
+            // 
+            this.BtnRespaldar.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BtnRespaldar.Location = new System.Drawing.Point(6, 26);
+            this.BtnRespaldar.Name = "BtnRespaldar";
+            this.BtnRespaldar.Size = new System.Drawing.Size(89, 33);
+            this.BtnRespaldar.TabIndex = 1;
+            this.BtnRespaldar.Text = "Respaldar";
+            this.BtnRespaldar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnRespaldar.UseVisualStyleBackColor = true;
+            this.BtnRespaldar.Click += new System.EventHandler(this.BtnRespaldar_Click);
             // 
             // BtnCancelar
             // 
@@ -162,31 +255,36 @@ namespace WindowsFormsUI.Formularios
             this.BtnCancelar.ForeColor = System.Drawing.Color.Black;
             this.BtnCancelar.Image = global::WindowsFormsUI.Properties.Resources.prohibition;
             this.BtnCancelar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.BtnCancelar.Location = new System.Drawing.Point(458, 374);
+            this.BtnCancelar.Location = new System.Drawing.Point(462, 403);
             this.BtnCancelar.Name = "BtnCancelar";
             this.BtnCancelar.Size = new System.Drawing.Size(103, 35);
-            this.BtnCancelar.TabIndex = 24;
+            this.BtnCancelar.TabIndex = 26;
             this.BtnCancelar.Text = "Cancelar";
             this.BtnCancelar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.BtnCancelar.UseVisualStyleBackColor = false;
             // 
-            // BtnCrearUsuario
+            // BtnGuardar
             // 
-            this.BtnCrearUsuario.BackColor = System.Drawing.Color.White;
-            this.BtnCrearUsuario.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(83)))), ((int)(((byte)(103)))));
-            this.BtnCrearUsuario.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(83)))), ((int)(((byte)(103)))));
-            this.BtnCrearUsuario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnCrearUsuario.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.BtnCrearUsuario.ForeColor = System.Drawing.Color.Black;
-            this.BtnCrearUsuario.Image = global::WindowsFormsUI.Properties.Resources.add_friend;
-            this.BtnCrearUsuario.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.BtnCrearUsuario.Location = new System.Drawing.Point(259, 374);
-            this.BtnCrearUsuario.Name = "BtnCrearUsuario";
-            this.BtnCrearUsuario.Size = new System.Drawing.Size(193, 35);
-            this.BtnCrearUsuario.TabIndex = 23;
-            this.BtnCrearUsuario.Text = "Guardar configuración";
-            this.BtnCrearUsuario.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.BtnCrearUsuario.UseVisualStyleBackColor = false;
+            this.BtnGuardar.BackColor = System.Drawing.Color.White;
+            this.BtnGuardar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(83)))), ((int)(((byte)(103)))));
+            this.BtnGuardar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(83)))), ((int)(((byte)(103)))));
+            this.BtnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnGuardar.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BtnGuardar.ForeColor = System.Drawing.Color.Black;
+            this.BtnGuardar.Image = global::WindowsFormsUI.Properties.Resources.add_friend;
+            this.BtnGuardar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnGuardar.Location = new System.Drawing.Point(263, 403);
+            this.BtnGuardar.Name = "BtnGuardar";
+            this.BtnGuardar.Size = new System.Drawing.Size(193, 35);
+            this.BtnGuardar.TabIndex = 25;
+            this.BtnGuardar.Text = "Guardar configuración";
+            this.BtnGuardar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BtnGuardar.UseVisualStyleBackColor = false;
+            // 
+            // OfdSeleccionar
+            // 
+            this.OfdSeleccionar.Filter = "Backup |*.bak";
+            this.OfdSeleccionar.Title = "Seleccionar archivo de respaldo";
             // 
             // FrmConfiguracion
             // 
@@ -194,6 +292,8 @@ namespace WindowsFormsUI.Formularios
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(577, 450);
+            this.Controls.Add(this.BtnCancelar);
+            this.Controls.Add(this.BtnGuardar);
             this.Controls.Add(this.TcOpciones);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "FrmConfiguracion";
@@ -202,6 +302,10 @@ namespace WindowsFormsUI.Formularios
             this.TpGeneral.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.TpBaseDatos.ResumeLayout(false);
+            this.TpBaseDatos.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -217,7 +321,16 @@ namespace WindowsFormsUI.Formularios
         private System.Windows.Forms.TextBox TxtRepetirClave;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar PbProgreso;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button BtnRespaldar;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button BtnRestaurar;
+        private System.Windows.Forms.Label LblPorcentaje;
+        private System.Windows.Forms.Label LblEstado;
         private System.Windows.Forms.Button BtnCancelar;
-        private System.Windows.Forms.Button BtnCrearUsuario;
+        private System.Windows.Forms.Button BtnGuardar;
+        private System.Windows.Forms.FolderBrowserDialog FbdUbicacion;
+        private System.Windows.Forms.OpenFileDialog OfdSeleccionar;
     }
 }
--- WindowsFormsUI/Formularios/FrmConfiguracion.cs
+++ WindowsFormsUI/Formularios/FrmConfiguracion.cs
@@ -5,6 +5,10 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.Data.SqlClient;
+using System.IO;
 
 namespace WindowsFormsUI.Formularios
 {
@@ -15,9 +19,115 @@ namespace WindowsFormsUI.Formularios
             InitializeComponent();
         }
 
-        private void groupBox1_Enter(object sender, EventArgs e)
+        private void BtnRespaldar_Click(object sender, EventArgs e)
         {
+            if (FbdUbicacion.ShowDialog() == DialogResult.OK)
+            {
+                PbProgreso.Value = 0;
 
+                try
+                {
+                    Server server = new Server(new ServerConnection("DESKTOP-2NF0HEH\\SQLEXPRESS", "sa", "_Mi@Sqlserver$Jevc&27!"));
+                    Backup backup = new Backup()
+                    {
+                        Action = BackupActionType.Database,
+                        Database = "AzocDb"
+                    };
+
+                    string archivo = string.Concat(FbdUbicacion.SelectedPath, "\\AzocDb-", DateTime.UtcNow.Hour, DateTime.UtcNow.Minute, DateTime.UtcNow.Second, ".bak");
+
+                    backup.Devices.AddDevice(archivo, DeviceType.File);
+                    backup.Initialize = true;
+                    backup.PercentComplete += Backup_PercentComplete;
+                    backup.Complete += Backup_Complete;
+                    backup.SqlBackup(server);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message + ex.Data + ex.InnerException, "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void Backup_Complete(object sender, ServerMessageEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                LblEstado.Invoke((MethodInvoker)delegate
+                {
+                    LblEstado.Text = e.Error.Message;
+                });
+            }
+        }
+
+        private void Backup_PercentComplete(object sender, PercentCompleteEventArgs e)
+        {
+            PbProgreso.Invoke((MethodInvoker)delegate
+            {
+                PbProgreso.Value = e.Percent;
+                PbProgreso.Update();
+            });
+
+            LblPorcentaje.Invoke((MethodInvoker)delegate
+            {
+                LblPorcentaje.Text = $"{e.Percent} %";
+            });
+        }
+
+        private void BtnRestaurar_Click(object sender, EventArgs e)
+        {
+            if (OfdSeleccionar.ShowDialog() == DialogResult.OK)
+            {
+                PbProgreso.Value = 0;
+
+                try
+                {
+                    Server server = new Server(new ServerConnection("DESKTOP-2NF0HEH\\SQLEXPRESS", "sa", "_Mi@Sqlserver$Jevc&27!"));
+                    Restore restore = new Restore()
+                    {
+                        Database = "AzocDb",
+                        Action = RestoreActionType.Database,
+                        ReplaceDatabase = true,
+                        NoRecovery = false
+                    };
+
+                    server.KillAllProcesses(restore.Database);
+                    restore.Devices.AddDevice(OfdSeleccionar.FileName, DeviceType.File);
+                    restore.PercentComplete += Restore_PercentComplete;
+                    restore.Complete += Restore_Complete;
+                    restore.SqlRestore(server);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message + ex.Data + ex.InnerException, "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void Restore_PercentComplete(object sender, PercentCompleteEventArgs e)
+        {
+            PbProgreso.Invoke((MethodInvoker)delegate
+            {
+                PbProgreso.Value = e.Percent;
+                PbProgreso.Update();
+            });
+
+            LblPorcentaje.Invoke((MethodInvoker)delegate
+            {
+                LblPorcentaje.Text = $"{e.Percent} %";
+            });
+        }
+
+        private void Restore_Complete(object sender, ServerMessageEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                LblEstado.Invoke((MethodInvoker)delegate
+                {
+                    LblEstado.Text = e.Error.Message;
+                });
+            }
         }
     }
 }
